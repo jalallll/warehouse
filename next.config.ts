99ffec7f451769e0465b86edf3d2ad18b0348e24
jalallll <jalalq11@gmail.com>
2024-12-28 09:58:52 -0500

    supabase integration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     env: {
         NEXT_PUBLIC_SERVER_PUBLIC_URL:
-            "https://warehouseapi-production.up.railway.app",
+            process.env.NEXT_PUBLIC_SERVER_PUBLIC_URL,
+        NEXT_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
 };
 
